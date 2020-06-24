16109142f281b2baf146dd1fe93cd105631d2d11
Lvv <2374060397@qq.com>
2020-06-24 20:39:03 +0800

    vuex and todo list

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import todos from './modules/todos' // 引入todos
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
+    todos
   }
 })
+// 本来可以把state和actions放下面的，为了美观和维护，就重新创建一个modules文件夹
